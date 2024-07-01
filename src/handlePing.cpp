da61cb684a1b79573eaa6b7258dfd0569b82762a
Oceane Rauline-Puig <orauline@c4r1p4.42perpignan.fr>
2024-07-01 16:27:50 +0200

    remise a zero du repo

--- src/handlePing.cpp
+++
@@ -1,8 +0,0 @@
-#include "../inc/Server.hpp"
-
-void	Server::handlePing(int index)
-{
-	std::string token = m_client[index].m_buffer.front();
-	sendMsgClient(index, "PONG " + token);
-	std::cout << CYAN << "PONG " << token << NC << std::endl;
-}
